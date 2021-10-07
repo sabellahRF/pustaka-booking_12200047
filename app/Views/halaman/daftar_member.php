ab7a061378f28f157e51c1490b5c6805d063c947
sabellahRF <12200047@gmail.com>
2021-10-07 03:56:20 -0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>daftar member</title>
+    </head>
+    <body>
+        <h1>Hai...Kamu mau menjadi member?</h1>
+        <p1>isikan data diri kamu dulu ya agar kami kenal dengan member kami</p1>
+    </body>
+</html>
