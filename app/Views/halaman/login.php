84a5aa3fac3d1b61650aedeca5e5d7bc7f161e5a
sabellahRF <12200047@bsi.ac.id>
2021-10-07 05:45:53 -0700

    tugas pertemuan 2

--- app/Views/halaman/login.php
+++ app/Views/halaman/login.php
@@ -1,18 +1,22 @@
-<?= $this->extend('dasar_tampilan'); ?>
-<?= $this->section('isiweb') ?>
-<form method="POST" action="<?= base_url('/login') ?>">
-    <div class="container col-md-3">
+<?= $this->extend('dasar_tampilan');?>
+<?= $this->section('isiweb')?>
+<form method="POST" action="<?=base_url('/login')?>">
+    <div class="container col-md-3"> 
         <div class="card mt-5">
             <div class="card-header">
                 <h3 class="card-title">Login</h3>
             </div>
             <div class="card-body">
                 <div class="form-floating">
-                    <input id="txtEmail" class="form-control" placeholder="Email" type="text" name="email" />
+                    <input id="txtEmail" class="form-control" 
+                            placeholder="Email" 
+                            type="text" name="email" />
                     <label for="txtEmail">Email</label>
                 </div>
                 <div class="form-floating">
-                    <input id="txtPass" class="form-control" placeholder="Password" type="password" name="sandi" />
+                    <input id="txtPass" class="form-control" 
+                            placeholder="Password" 
+                            type="password" name="sandi" />
                     <label for="txtPass">Kata Sandi</label>
                 </div>
                 <button class="btn btn-primary mt-2">Login</button>
@@ -20,4 +24,5 @@
         </div>
     </div>
 </form>
-<?= $this->endSection() ?>
+
+<?= $this->endSection()?>
